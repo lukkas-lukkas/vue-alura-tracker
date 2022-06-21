7ec48cb7cd8925df27811e0ac041395b3807e441
Lucas Lima <lukkasoliveiralima@gmail.com>
2022-06-21 00:44:18 -0300

    abstract timer

--- src/components/TaskForm.vue
+++ src/components/TaskForm.vue
@@ -5,21 +5,7 @@
                 <input type="text" class="input" placeholder="What task do you want start?">
             </div>
             <div class="column">
-                <div class="is-flex is-align-items-center is-justify-content-space-between">
-                    <TimerDisplay :timeInSeconds="timeInSeconds"/>
-                    <button class="button" @click="startTimer">
-                        <span class="icon">
-                            <i class="fas fa-play"></i>
-                        </span>
-                        <span>play</span>
-                    </button>
-                    <button class="button" @click="stopTimer">
-                        <span class="icon">
-                            <i class="fas fa-stop"></i>
-                        </span>
-                        <span>stop</span>
-                    </button>
-                </div>
+                <Timer />
             </div>
         </div>
     </div>
@@ -27,30 +13,10 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import TimerDisplay from "./TimerDisplay.vue";
+import Timer from "./Timer.vue";
 
 export default defineComponent({
     name: "TaskForm",
-    //Default value to property
-    data() {
-        return {
-            timeInSeconds: 0,
-            timer: 0
-        };
-    },
-    methods: {
-        startTimer() {
-            this.timer = setInterval(() => {
-                this.timeInSeconds += 1;
-            }, 1000);
-            console.log("Starting timer now");
-        },
-        stopTimer() {
-            clearInterval(this.timer);
-            this.timeInSeconds = 0;
-            console.log("Stoping timer now");
-        }
-    },
-    components: { TimerDisplay }
+    components: { Timer }
 })
 </script>
---
+++ src/components/Timer.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="is-flex is-align-items-center is-justify-content-space-between">
+        <TimerDisplay :timeInSeconds="timeInSeconds"/>
+        <button class="button" @click="startTimer">
+            <span class="icon">
+                <i class="fas fa-play"></i>
+            </span>
+            <span>play</span>
+        </button>
+        <button class="button" @click="stopTimer">
+            <span class="icon">
+                <i class="fas fa-stop"></i>
+            </span>
+            <span>stop</span>
+        </button>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import TimerDisplay from "./TimerDisplay.vue";
+
+export default defineComponent({
+    name: "Timer",
+    //Default value to property
+    data() {
+        return {
+            timeInSeconds: 0,
+            timer: 0
+        };
+    },
+    methods: {
+        startTimer() {
+            this.timer = setInterval(() => {
+                this.timeInSeconds += 1;
+            }, 1000);
+            console.log("Starting timer now");
+        },
+        stopTimer() {
+            clearInterval(this.timer);
+            this.timeInSeconds = 0;
+            console.log("Stoping timer now");
+        }
+    },
+    components: { TimerDisplay }
+})
+</script>
