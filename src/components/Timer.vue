efbe86a5230bd70c626b3e0b593d21cef652085d
Lucas Lima <lukkasoliveiralima@gmail.com>
2022-06-21 00:51:26 -0300

    add state to timer buttons

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="is-flex is-align-items-center is-justify-content-space-between">
         <TimerDisplay :timeInSeconds="timeInSeconds"/>
-        <button class="button" @click="startTimer">
+        <button class="button" @click="startTimer" :disabled="timerRunning">
             <span class="icon">
                 <i class="fas fa-play"></i>
             </span>
             <span>play</span>
         </button>
-        <button class="button" @click="stopTimer">
+        <button class="button" @click="stopTimer" :disabled="!timerRunning">
             <span class="icon">
                 <i class="fas fa-stop"></i>
             </span>
@@ -26,17 +26,20 @@ export default defineComponent({
     data() {
         return {
             timeInSeconds: 0,
-            timer: 0
+            timer: 0,
+            timerRunning: false
         };
     },
     methods: {
         startTimer() {
+            this.timerRunning = true;
             this.timer = setInterval(() => {
                 this.timeInSeconds += 1;
             }, 1000);
             console.log("Starting timer now");
         },
         stopTimer() {
+            this.timerRunning = false;
             clearInterval(this.timer);
             this.timeInSeconds = 0;
             console.log("Stoping timer now");
