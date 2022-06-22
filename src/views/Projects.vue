eb232ca180ee3f17e056418eaa295d705f51a43f
Lucas Lima <lukkasoliveiralima@gmail.com>
2022-06-21 23:59:28 -0300

    create form and first behavior

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,11 +1,50 @@
 <template>
-    <h1>Projetos</h1>
+    <section class="projects">
+        <h1 class="title">Projetos</h1>
+        <form @submit.prevent="save">
+            <div class="field">
+                <label for="projectName" class="label">
+                    Project Name
+                </label>
+                <input type="text" class="input" v-model="projectName" id="projectName">
+            </div>
+            <div class="field">
+                <button class="button" type="submit">
+                    Save
+                </button>
+            </div>
+        </form>
+    </section>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import IProject from "../interfaces/IProject";
 
 export default defineComponent({
-    name: 'Projects'
+    name: 'Projects',
+    data() {
+        return {
+            projectName: '',
+            projects: [] as IProject[]
+        }
+    },
+    methods: {
+        save() {
+            const project: IProject = {
+                id: new Date().toISOString(),
+                name: this.projectName
+            }
+
+            this.projects.push(project);
+            this.projectName = '';
+        }
+    }
 })
 </script>
+
+<style scoped>
+.projects {
+    padding: 1.25rem;
+}
+</style>
