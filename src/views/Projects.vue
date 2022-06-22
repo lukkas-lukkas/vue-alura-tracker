969b6cee1bee571e2321753f91d0f28a280d19a2
Lucas Lima <lukkasoliveiralima@gmail.com>
2022-06-22 00:06:06 -0300

    create table to projects

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -14,6 +14,20 @@
                 </button>
             </div>
         </form>
+        <table class="table is-fullwidth">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>NAME</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="project in projects" :key="project.id">
+                    <td>{{ project.id }}</td>
+                    <td>{{ project.name }}</td>
+                </tr>
+            </tbody>
+        </table>
     </section>
 </template>
 
